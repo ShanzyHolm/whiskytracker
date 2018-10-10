1645adaafebbd95029175d86cc08a5c5fda3ebc3
Shannon Holmes <snholmes13@gmail.com>
2018-10-11 00:57:46 +0100

    Initial Commit

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryImpl.java
@@ -0,0 +1,44 @@
+package com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+
+public class WhiskyRepositoryImpl implements WhiskyRepositoryCustom {
+
+    @Autowired
+    EntityManager entityManager;
+
+//  TODO:   * get all the whiskies for a particular year
+
+    @Transactional
+    public List<Whisky> getWhiskiesForParticularYear(int year) {
+        List<Whisky> whiskiesFound = null;
+        Session session = entityManager.unwrap (Session.class);
+
+        try {
+            Criteria cr = session.createCriteria (Whisky.class);
+            cr.add (Restrictions.eq ("year", year));
+            whiskiesFound = cr.list ();
+        } catch (HibernateException e) {
+            e.printStackTrace ();
+        } finally {
+            session.close ();
+        }
+
+        return whiskiesFound;
+    }
+
+//  TODO:   * get all the whisky from a particular region
+//  TODO:   * get all the distilleries for a particular region
+//  TODO:   * get all the whisky from a particular distillery that's a specific age (if any)
+//  TODO:   * Get disilleries that have whiskies that are 12 years old
+}
